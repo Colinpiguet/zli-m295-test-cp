d8bb69ca3c908a3f9fa2e5b411bf2f335180a55e
Colinpiguet <piguetcolin@gmail.com>
2023-12-22 10:09:54 +0000

    created the patch and delete enpoints, but the delete is not working till now

--- UK_test/Hauptauftraege/tasks.js
+++ UK_test/Hauptauftraege/tasks.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-const-assign */
+/* eslint-disable no-return-assign */
 /* eslint-disable no-shadow */
 //  untere zeile macht random id
 const { randomUUID } = require('node:crypto');
@@ -13,25 +15,53 @@ app.use(express.json());
 // leerer Array für die Tasks
 const tasks = [];
 
-// GET /tasks Endpunkt mit Statuscode 200
+// GET /tasks Endpunkt mit get mehtod und mit Statuscode 200
 app.get('/tasks', (request, response) => {
   response.status(200).json(tasks);
 });
 
-// POST /tasks Endpunkt mit Statuscode 201 und einer random id
+// POST /tasks Endpunkt mit push mehtod und mit Statuscode 201 und einer random id
 app.post('/tasks', (request, response) => {
   const newTask = { ...request.body, id: randomUUID() };
   tasks.push(newTask);
   response.status(201).json(newTask);
 });
 
-// GET /tasks/:id Endpunkt mit Statuscode 200 und bei error 404
+// GET /tasks/:id Endpunkt mit mit find method und mit Statuscode 200 und bei error 404
 app.get('/tasks/:id', (request, response) => {
   const task = tasks.find((task) => task.id === request.params.id);
   if (task) {
     response.status(200).json(task);
   } else {
-    response.status(404).json({ error: 'Aufgabe wurde nich gefunden' });
+    response.status(404).json({ error: 'Task wurde nich gefunden' });
+  }
+});
+
+// PATCH /tasks/:id Endpunkt mit find mehtod und mit Statuscode 200 und bei error 404
+app.patch('/tasks/:id', (request, response) => {
+  const taskId = request.params.id;
+  const keys = Object.keys(request.body);
+  const taskToUpdate = tasks.find((task) => task.id === taskId);
+
+  if (taskToUpdate) {
+    keys.forEach((key) => taskToUpdate[key] = request.body[key]);
+    response.status(200).json(tasks);
+  } else {
+    response.status(404).json({ error: 'Task wurde nicht gefunden.' });
+  }
+});
+
+// DELETE /tasks/:id Endpunkt mit filter mehtod und mit Statuscode 200 und bei error 404
+// initialLenght überprüft ob die neue task kleiner ist als die alte Task, welche man löschen will
+app.delete('/tasks/:id', (request, response) => {
+  const taskId = request.params.id;
+  const initialLength = tasks.length;
+  tasks = tasks.filter((task) => task.id !== taskId);
+
+  if (tasks.length !== initialLength) {
+    response.status(200).json({ message: 'Task wurde erfolgreich gefunden und gelöscht' });
+  } else {
+    response.status(404).json({ error: 'Task wurde nicht gefunden' });
   }
 });
 
