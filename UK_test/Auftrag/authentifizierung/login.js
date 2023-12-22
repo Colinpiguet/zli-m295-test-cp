5475953b7db196a5a1ecf18444e81f88b2ee792d
Colinpiguet <piguetcolin@gmail.com>
2023-12-22 12:08:08 +0000

    POST /login enpoint is now working, also tested

--- UK_test/Auftrag/authentifizierung/login.js
+++ UK_test/Auftrag/authentifizierung/login.js
@@ -1,27 +1,29 @@
 const express = require('express')
 const session = require('express-session')
 const app = express()
+const port = 3000;
 
 app.use(express.json())
 app.use(session({
   secret: 'supersecret',
-	resave: false,
-	saveUninitialized: false,
+  resave: false,
+  saveUninitialized: false,
   cookie: {}
 }))
 
+// POST /login Endpunkt mit Statuscode 200 und bei error 401
 app.post('/login', function (request, response) {
-	const { email, password } = request.body
-	if (email?.toLowerCase() == secretAdminCredentials.email && password == secretAdminCredentials.password) {
-		request.session.email = email
-		return response.status(200).json({ email: request.session.email })
-	}
-  return response.status(401).json({ error: "Ungültige credentials" })
+  const { email, password } = request.body
+  //Hier wird nur überprüft ob das passwort m295 ist, die email aber nicht, da jede email funtionieren sollte
+  if (password === 'm295') {
+    request.session.email = email
+    return response.status(200).json({ email: request.session.email })
+  }
+  return response.status(401).json({ error: 'Invalid credentials' })
 })
 
 // Server
 app.listen(port, () => {
-    // eslint-disable-next-line no-console
-    console.log(`Server listening on port ${port}`);
-  });
+  console.log(`Server listening on port ${port}`)
+})
   
