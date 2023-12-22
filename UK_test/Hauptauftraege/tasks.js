d4949fa189ecea6b44db1589b7e6d4197cbed388
Colinpiguet <piguetcolin@gmail.com>
2023-12-22 09:22:00 +0000

    GET/ tasks/:id endpoint created

--- UK_test/Hauptauftraege/tasks.js
+++ UK_test/Hauptauftraege/tasks.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-shadow */
+//  untere zeile macht random id
 const { randomUUID } = require('node:crypto');
 const express = require('express');
 
@@ -16,11 +18,21 @@ app.get('/tasks', (request, response) => {
   response.status(200).json(tasks);
 });
 
-// POST /tasks Endpunkt mit Statuscode 201
+// POST /tasks Endpunkt mit Statuscode 201 und einer random id
 app.post('/tasks', (request, response) => {
   const newTask = { ...request.body, id: randomUUID() };
   tasks.push(newTask);
   response.status(201).json(newTask);
+});
+
+// GET /tasks/:id Endpunkt mit Statuscode 200 und bei error 404
+app.get('/tasks/:id', (request, response) => {
+  const task = tasks.find((task) => task.id === request.params.id);
+  if (task) {
+    response.status(200).json(task);
+  } else {
+    response.status(404).json({ error: 'Aufgabe wurde nich gefunden' });
+  }
 });
 
 // Server
